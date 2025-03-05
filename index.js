1e70be28b887794fe64cd3e63e8373fed36484ec
rejo132 <enoch.luke1@student.moringaschool.com>
2025-03-05 13:25:25 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = (31 * 2);
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 24;
+let num4 =10;
+const mod = (24 % 10)
+console.log(mod);
+
+const max = Math.max(5, 10, 15 , 20);
+console.log(max);
